9025b8153a6e290be1a61ff91271adbe6ccd5d7f
jvictornascimento <victojoao789@gmail.com>
2025-09-05 19:53:07 -0300

    Create the product image component

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -3,8 +3,13 @@ import Hero from "../hero/Hero.jsx";
 import Paginator from "../commom/Paginator.jsx";
 import {Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import ProductImage from "../utils/ProductImage.jsx";
 
 const Home = () => {
+    const [product, setProduct] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+
     const [currentPage, setCurrentPage] = useState([]);
     const itemsPerPage = 10;
 
@@ -18,30 +23,41 @@ const Home = () => {
     );
 
     return (
-        <div>
+        <>
             <Hero/>
+            <div className='d-flex flex-wrap justify-content-center p-5'>
+                {product.map((product) => (
+                    <Card key={product.id} className='home-product-card'>
+                        <Link to={"#"} className='link'>
+                            <div className='image-container'>
+                                {product.images.length > 0 && (
+                                    <ProductImage productId={product.images[0].id}/>
+                                )}
+                            </div>
+                        </Link>
+                            <Card.Body>
+                                <p className="product-description">
+                                    {product.name} - {product.description}
+                                </p>
+                                <h4 className='price'>{product.price}</h4>
+                                <p className='text-success'>{product.inventory} in stock.</p>
+                                <Link to={`/products/${product.name}`} className='shop-now-button'>
+                                    {" "}
+                                    Shop now
+                                </Link>
+                            </Card.Body>
+                        </Card>
+                ))}
 
-            <Card className='home-product-card'>
-                <Link to={"#"}>Product image will be were...</Link>
-                <Card.Body>
-                    <p className="product-description">
-                        product name and description will be were...
-                    </p>
-                    <h4 className='price'>product price</h4>
-                    <p className='text-success'>Product inventory in stock.</p>
-                    <Link to={"#"} className='shop-now-button'>
-                        {" "}
-                        Shop now
-                    </Link>
-                </Card.Body>
-            </Card>
-            <Paginator
-                itemsPerPage={itemsPerPage}
-                totalItems={filteredProducts.length}
-                currentPage={currentPage}
-                paginate={paginate}
-            />
-        </div>
+                        <Paginator
+                            itemsPerPage={itemsPerPage}
+                            totalItems={filteredProducts.length}
+                            currentPage={currentPage}
+                            paginate={paginate}
+                        />
+
+            </div>
+        </>
     );
 };
 
